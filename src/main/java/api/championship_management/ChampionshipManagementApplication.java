2ad9d10a5f61c2d4cdd13a78ef2836712305f6f3
andryraz <tiavinarazafindratsimba20@gmail.com>
2025-04-28 12:12:30 +0300

    feat: Adding functionality springboot

---
+++ src/main/java/api/championship_management/ChampionshipManagementApplication.java
@@ -0,0 +1,13 @@
+package api.championship_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampionshipManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChampionshipManagementApplication.class, args);
+    }
+
+}
